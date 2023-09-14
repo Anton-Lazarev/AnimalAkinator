1add4cddf42d683a55e6a1077f13ef392daeb2d6
Anton-Lazarev <big.panci@yandex.ru>
2023-09-14 17:07:33 +0300

    fix : unused import after catching exception

--- src/main/java/ru/animal/akinator/AnimalAkinator.java
+++ src/main/java/ru/animal/akinator/AnimalAkinator.java
@@ -2,10 +2,8 @@ package ru.animal.akinator;
 
 import ru.animal.akinator.service.AnimalAkinatorService;
 
-import java.io.IOException;
-
 public class AnimalAkinator {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         AnimalAkinatorService akinatorService = new AnimalAkinatorService();
         akinatorService.startGame();
     }
